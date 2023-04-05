cc0e26839b9926574c106d7b028a92012b53c511
brian-ikiara <brnikiara@gmail.com>
2023-04-05 11:02:32 +0300

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,32 +1,52 @@
 #include "binary_trees.h"
 
 /**
+ * swap_nodes - Function
+ * @swpe: Swapee
+ * @swpr: Swaper
+ *
+ * Description: Swaps 2 Binary Tree nodes.
+ * Return: swpr, the new node; NULL, otherwise.
+ * On error, stderr.
+ */
+binary_tree_t *swap_nodes(binary_tree_t *swpe, binary_tree_t *swpr)
+{
+	binary_tree_t *temp = malloc(sizeof(binary_tree_t));
+
+	if (temp == NULL)
+		return (NULL);
+	temp = swpe, swpe = swpr, swpr = temp;
+
+	return (swpr);
+}
+
+/**
  * binary_tree_insert_left - Function
  * @parent: Parent node
  * @value: Node data
  *
  * Description: Inserts a node to as a left child of
  * another node.
- * Return: newLeft, the new node; NULL, otherwise.
+ * Return: new, the new node; NULL, otherwise.
  * On error, stderr.
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newLeft = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new = (binary_tree_t *) malloc(sizeof(binary_tree_t));
 
-	if (parent == NULL)
+	if (new == NULL)
 		return (NULL);
-	if (newLeft == NULL)
-		return (NULL);
-	newLeft = binary_tree_node(parent, value);
-	if (parent->left == NULL)
-		parent->left = newLeft;
-	else
+	new->parent = parent, new->n = value, new->right = NULL;
+	if (parent != NULL)
 	{
-		newLeft->left = parent->left;
-		parent->left = newLeft;
-		newLeft->left->parent = newLeft;
-	}
+		if (parent->left != NULL)
+			new->left = parent->left, parent->left->parent = new;
+		else
+			new->left = NULL;
+		parent->left = new;
 
-	return (newLeft);
+		return (new);
+	}
+	new->left = NULL;
+	return (NULL);
 }
