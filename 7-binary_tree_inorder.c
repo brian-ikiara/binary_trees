f91e7fd3cc55506497239e7abaf7a68b2b941a1f
brian-ikiara <brnikiara@gmail.com>
2023-04-05 13:59:34 +0300

    Create 7-binary_tree_inorder.c

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - Function
+ * @tree: Binary Tree
+ * @func: Function pointer
+ *
+ * Description: Traverses the Binary Tree using the
+ * inorder technique: all left nodes, then root and,
+ * finally, all the right nodes. Once again, recursion
+ * is preferred over a loop.
+ * Return: void.
+ * On error, stderr.
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	binary_tree_preorder(tree->left, func);
+	func(tree->n);
+	binary_tree_preorder(tree->right, func);
+}
