4fbcbc95ffcea452fb8441bd67905096eb996c1e
brian-ikiara <brnikiara@gmail.com>
2023-04-05 14:01:12 +0300

    Fix 7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -12,7 +12,7 @@
  * Return: void.
  * On error, stderr.
  */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
